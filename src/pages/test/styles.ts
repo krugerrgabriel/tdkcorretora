aefbc0469df291141691d59c5d48180bc9e35e5b
Gabriel Sabara Krüger <gabriel.sa.kruger@gmail.com>
2021-05-04 18:03:58 -0300

    some updates

--- src/pages/test/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    background-color: ${props => props.theme.background};
-`;
-
-export const Text = styled.p`
-    background-color: ${props => props.theme.primary};
-`;
